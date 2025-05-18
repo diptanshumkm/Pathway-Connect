563a7da7e30ddf0a69a9f1aa2269fe7f797470a4
diptanshumkm <diptanshu.uae@gmail.com>
2025-05-18 21:58:40 +0530

    Update

--- Backend/backend/src/main/java/pathwayconnect/example/backend/Models/Goals.java
+++ Backend/backend/src/main/java/pathwayconnect/example/backend/Models/Goals.java
@@ -1,10 +1,25 @@
 package pathwayconnect.example.backend.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "goals_table")
-public class Goals {
+public class UserProfile {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false, length = 50)
+    private String fullName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "bio", nullable = false, length = 50)
+    private String bio;
+
     
+    @Column(name = "bio", nullable = false, length = 50)
+    private String phoneNumber;
+
 }
